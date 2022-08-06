b11ae71a460375c6ca3c1b33cc8e26536ef1f4e1
Pravendra Kumar <kumar.pravendra@thinksys.com>
2022-08-06 14:00:33 +0530

    add initial files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css'
+import './assets/css/responsive.css'
+import './assets/css/jquery.mCustomScrollbar.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
